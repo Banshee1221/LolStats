d084f1eb62021f3eef57a8e22e9bf08d9d6b9fee
Eugene <banshee1221@gmail.com>
2015-03-01 19:21:37 +0200

    a lot of stuff

--- APITEST.py
+++ APITEST.py
@@ -11,14 +11,42 @@ w = RiotWatcher(key)
 s = w.get_summoner(name=summoner_name, region='euw')
 sID = s['id']
 
-stats = json.dumps(w.get_match_history(summoner_id=sID, region='euw'))
-parsed = json.loads(stats)
-print "Amount of games: "+str(json.dumps(parsed, indent=4, sort_keys=True).count("matchId"))
-print "Data:"
-print json.dumps(parsed, indent=4, sort_keys=True)
+match = 1960675310
 
-
-print "==== Match details for 1960675310 ===="
-stats1 = json.dumps(w.get_match(1960675310, region='euw', include_timeline='true'))
+print "==== Match details for", str(match), "===="
+stats1 = json.dumps(w.get_match(match, region='euw', include_timeline='true'))
 parsed1 = json.loads(stats1)
-print json.dumps(parsed1, indent=4, sort_keys=True)
+#print json.dumps(parsed1, indent=4, sort_keys=True)
+#for key, value in parsed1.items():
+#    print key, value
+
+# pretty print
+_matchId = parsed1['matchId']
+
+_mapId = parsed1['mapId']
+
+_people = {}                                                                # ID | name
+
+for a in parsed1['participantIdentities']:
+    _people[a['player']['summonerId']] = str(a['player']['summonerName'])
+
+_participants = {}                                                          # number | ID
+
+for b in parsed1['participantIdentities']:
+    _participants[b['participantId']] = b['player']['summonerId']
+
+_eventsPerPerson = {}
+
+for count in range(10):
+    _eventsPerPerson[str(count + 1)] = []
+
+
+for c in parsed1['timeline']['frames']:
+    for count in range(1, 11):
+        print count
+        temp = _eventsPerPerson[str(count)]
+        tempDict = dict(c['participantFrames'][str(count)])
+        _eventsPerPerson[str(count)] = _eventsPerPerson[str(count)].append(tempDict)
+        #temp = temp.append(c['participantFrames'][str(count)])
+        #print temp
+        #_eventsPerPerson[count] = temp

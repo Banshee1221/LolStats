1653e2d697233f1d34268d048775412604f2b370
Eugene <banshee1221@gmail.com>
2015-03-03 17:06:40 +0200

    Heatmap implementation

--- LolStatsWeb.py
+++ LolStatsWeb.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 # Imports
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 import requests
 import json
 from main import main
@@ -9,18 +10,32 @@ from main import main
 # LolStats
 
 run = main(str(raw_input('Enter API Key: ')), "StirlingArcher69", 1960675310)
-_events = run.getEventsPerPerson()
+
+def sortEvents(personID):
+    event = []
+    x = []
+    y = []
+    time = []
+    _events = run.getEventsPerPerson()
+    for events in _events[str(personID)]:
+        if 'position' in events:
+            event.append(events['eventType'])
+            x.append((int(events['position']['x'])*14990/14820 - 120)*512/14990 - 120)
+            y.append((int(events['position']['y'])*15100/14881 - 120)*512/15040 - 120)
+            time.append(events['timestamp'])
+    for i in range(len(event)):
+        event[i] = str(event[i]).replace("u", "")
+    print x, y
+
+    return event, x, y, time
 
 # Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    headers = {'Content-Type': 'application/json'}
-    url = 'http://httpbin.org/post'
-    r = requests.post(url, data=json.dumps(_events), headers=headers)
-    return json.dumps(r.json(), indent=4)
-    #return render_template('home.html')
+    event, x, y, time = sortEvents(1)
+    return render_template('home.html', events=event, x=x, y=y, time=time)
 
 if __name__ == '__main__':
     app.run()

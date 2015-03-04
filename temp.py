3de42d31c471f18dc4679c4f7c06cf1cff06291d
Eugene <banshee1221@gmail.com>
2015-03-04 20:26:33 +0200

    Initiate DB implementation prep

--- temp.py
+++ temp.py
@@ -11,25 +11,25 @@ print _sumObj
 
 #_parsed=_watcherObj.get_match_history(summoner_id=_sumObj['id'],region='euw')
 #print json.dumps(_parsed, indent=4)
-_parsed1=_watcherObj.get_match(match_id=1997269764, region='euw', include_timeline=True)
-#print json.dumps(_parsed1, indent=4)
+_parsed1=_watcherObj.get_match(match_id=1997209490, region='euw', include_timeline=True)
+print json.dumps(_parsed1, indent=4)
 
-_eventsPerPerson = {}
-
-for count in range(10):
-    _eventsPerPerson[str(count + 1)] = []
-
-for items in _parsed1['timeline']['frames']:     # ignores wards
-    try:
-        tempList = items['events']
-        for vals in tempList:
-           #if "participantId" in str(vals):
-           #    _eventsPerPerson[str(vals['participantId'])] = _eventsPerPerson[str(vals['participantId'])] + [vals]
-            if "killerId" in str(vals) and "BUILDING_KILL" not in str(vals):
-                _eventsPerPerson[str(vals['killerId'])] = _eventsPerPerson[str(vals['killerId'])] + [vals]
-    except KeyError:
-        continue
-        # print "skipped", str(items['participantFrames'])
-
-for items in _eventsPerPerson['5']:
-    print items
+# _eventsPerPerson = {}
+#
+# for count in range(10):
+#     _eventsPerPerson[str(count + 1)] = []
+#
+# for items in _parsed1['timeline']['frames']:     # ignores wards
+#     try:
+#         tempList = items['events']
+#         for vals in tempList:
+#            #if "participantId" in str(vals):
+#            #    _eventsPerPerson[str(vals['participantId'])] = _eventsPerPerson[str(vals['participantId'])] + [vals]
+#             if "killerId" in str(vals) and "BUILDING_KILL" not in str(vals):
+#                 _eventsPerPerson[str(vals['killerId'])] = _eventsPerPerson[str(vals['killerId'])] + [vals]
+#     except KeyError:
+#         continue
+#         # print "skipped", str(items['participantFrames'])
+#
+# for items in _eventsPerPerson['5']:
+#     print items

95343fecf338f02a45ff3438e8109faddc172e94
Eugene <banshee1221@gmail.com>
2015-03-17 18:15:01 +0200

    Implement checkboxes and fix the overloay modal

--- static/js/init.js
+++ static/js/init.js
@@ -4,4 +4,13 @@ $(document).ready(function () {
             $('#butChange').html("<span class='glyphicon glyphicon-refresh glyphicon-refresh-animate'></span> Loading...")
         }
     });
+    $('input[type=checkbox]').change(
+    function(){
+        if (this.checked) {
+            alert('checked');
+        }
+        else {
+            alert('unchecked');
+        }
+    });
 });
